eeadce7a6533832a86df6e802a26d7e122403de6
top8887 <topolnytskyj@ukr.net>
2026-06-14 17:31:51 +0000

    feat: add LeafNode class with to_html rendering and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -37,18 +37,22 @@ class TestHTMLNode(unittest.TestCase):
             None,
         )
 
-    def test_repr(self):
-        node = HTMLNode(
-            "p",
-            "What a strange world",
-            None,
-            {"class": "primary"},
-        )
-        self.assertEqual(
-            node.__repr__(),
-            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
-        )
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
+    def test_leaf_to_html_p_noteq(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertNotEqual(node.to_html(), "<p>Hello, guys!</p>")
+
+    def test_leaf_to_html_p_eq_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_to_html_p_noteq_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertNotEqual(node.to_html(), '<a href="https://www.google.com"></a>')
+    
 
 if __name__ == "__main__":
     unittest.main()
