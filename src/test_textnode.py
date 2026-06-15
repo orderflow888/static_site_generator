eb877de590aa07ab3228aa3dfbaa4510f296ad96
top8887 <topolnytskyj@ukr.net>
2026-06-15 21:28:27 +0000

    Implement TextNode to HTMLNode conversion and add tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -24,6 +24,12 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD, 'www.google.com')
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
+        
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -26,3 +27,20 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+    
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    if text_node.text_type not in TextType:
+        raise ValueError(f"Invalid text type: {text_node.text_type}")
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode('b', text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode('i', text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode('code', text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode('a', text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", str(), {"src": text_node.url, "alt" : text_node.text})
